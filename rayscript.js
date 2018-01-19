65b4303d51d3176ac96416c0d351d9aaf36694d6
Raymond Raps <raymondraps@Raymonds-MacBook-Pro.local>
2018-01-18 18:02:40 -0800

    Fixing js
    Fixing the javascript. Why won't it work?!

--- rayscript.js
+++ rayscript.js
@@ -1,4 +1,4 @@
 //This will calculate how many visitors came to this site.
 
-var imglist = ["images/Psycho Dad.jpg", "images/Angry Austrailian Dad.jpg"];
-document.getElementById("rand").src = imglist[1];
+var copyright = 2018;
+document.getElementById('copyright').innerHTML = copyright;
