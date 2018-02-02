8851309cf9b7e43bb6649f7d7ebe5c9b331e68fd
Raymond Raps <raymondraps@Raymonds-MacBook-Pro.local>
2018-02-02 15:51:43 -0800

    Major Change
    Major Change is in charge!

--- rayscript.js
+++ rayscript.js
@@ -1,3 +1,10 @@
 //This will calculate how many visitors came to this site.
 
-document.getElementById('copyright').innerHTML = "2018";
+//document.getElementById('copyright').innerHTML = 2018;
+
+var visit = 0;
+
+function getVisit() {
+	visit = visit + 1;
+	document.getElementById('visit').innerHTML = visit;
+}
