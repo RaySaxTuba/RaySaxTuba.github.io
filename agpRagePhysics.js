c8f272dd6743d0a464dd681f9c66e30ee6adb816
RaySaxTuba <rjraps@gmail.com>
2018-09-21 23:50:53 -0700

    The tuba is loud Bob Saget!
    :alien:  Amm ficksing the website tuba!

---
+++ agpRagePhysics.js
@@ -0,0 +1,7 @@
+// This is a physical Angry Grandpa rage tuba thing!
+var agp = new Image();
+agp.src = "images/agp.jpg";
+
+var canvas = document.getElementById("agpRagePhysics");
+var ctx = canvas.getContext('2d');
+ctx.drawImage(agp,0,0,100,100);
