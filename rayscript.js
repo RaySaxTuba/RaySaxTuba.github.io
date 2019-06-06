ec139111d384211614bad1a3a698facc7d4824c1
RaySaxTuba <rjraps@gmail.com>
2019-06-06 13:24:36 -0700

    Script test
    Testing the js script on my website.

--- rayscript.js
+++ rayscript.js
@@ -1,12 +1,15 @@
 // this is the javascript for the website
-
+/*
 document.getElementsByTagName("nav").innerHTML =
 "<ul>
 <li><a href=\'index.html\'>Home</a></li>
-<li><a href=\"raybio.html\">About Me</a></li>
-<li><a href=\"images.html\">My images</a></li>
-<li><a href=\"music.html\">My music</a></li>
-<li><a href=\"video.html\">My videos</a></li>
-<li><a href=\"quotes.html\">Favorite Funny Quotes</a></li>
+<li><a href=\'raybio.html\'>About Me</a></li>
+<li><a href=\'images.html\'>My images</a></li>
+<li><a href=\'music.html\'>My music</a></li>
+<li><a href=\'video.html\'>My videos</a></li>
+<li><a href=\'quotes.html\'>Favorite Funny Quotes</a></li>
 <li><a href=\'software.html\'>Ray\'s apps</a></li>
 </ul>";
+*/
+
+window.alert("Mah candy!");
