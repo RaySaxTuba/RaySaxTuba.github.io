da422c0752a93ac159c27cbb68875e963ea4c268
RaySaxTuba <rjraps@gmail.com>
2019-07-14 17:25:14 -0700

    Add app
    Adding an app.

---
+++ apps/JS_Test/JS_Test.js
@@ -0,0 +1,29 @@
+let agp;
+//let mahCandy;
+
+function preload() {
+  soundFormats('wav', 'mp3');
+  agp = loadImage("data/AngryGrampa.jpg");
+  //mahCandy = loadSound("data/MAH candy!.wav");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  imageMode(CENTER);
+  textAlign(CENTER);
+  // Make volume get higher as the sound loops!
+  //mahCandy.setVolume(0.1);
+  // Multiple quotes!
+  // Make the "I've had it with you!" quote!
+  //mahCandy.play();
+}
+
+function draw() {
+  background("red");
+  translate(width/2, height/2);
+  rotate(millis()/1000);
+  image(agp, 0, 0, 100, 100);
+  rotate(millis()*2/1000);
+  text("Lesbian Burgers!", 0, 0);
+}
+// Make an app that has your audio quotes in it!
