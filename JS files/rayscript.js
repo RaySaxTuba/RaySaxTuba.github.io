b60d9452f9b6c7e0d09388ead38e7579eeb88153
RaySaxTuba <rjraps@gmail.com>
2019-07-20 16:39:47 -0700

    Welcome to back!
    Yay I'm back in full frigging force!

---
+++ JS files/rayscript.js
@@ -0,0 +1,59 @@
+// this is the javascript for the website
+
+// This is the copyright updater code
+var year = 2019;
+// These are quotes from people including me!
+var rayQuotes = [
+  'The tuba is loud!',
+  'The force is strong with mah stinky breath!',
+  'Yay chuba yay!',
+];
+
+var agpQuotes = [
+  'Mah candy!',
+
+  '"Can\'t get no sleep!' +
+  'Can\'t get no rest!' +
+  'Can\'t get no respect!"',
+
+  '"I\'ve had it with you!' +
+  'Ain\'t nothing left to tear up!' +
+  'The kitchen, has finally closed!!"'
+];
+
+var animeQuotes = [
+  '"It\'s over 9000!!!!" -- Vegeta'
+];
+
+var quoteList = [
+  rayQuotes,
+  agpQuotes,
+  animeQuotes
+];
+
+document.getElementById('copyright').innerHTML = year;
+
+function quoteChoose() {
+  var sayer, chosen;
+  var chooser = Math.floor(random()*quoteList.length);
+
+  switch (chooser) {
+    case 0:
+    chosen = quoteList[chooser];
+    sayer = "Me!";
+      break;
+    case 1:
+    chosen = quoteList[chooser];
+    sayer = "Angry Grandpa";
+      break;
+    case 2:
+    chosen = quoteList[chooser];
+    sayer = "";
+      break;
+  }
+  var postChoose = Math.floor(random()*chosen.length);
+  document.getElementById('footQuotes').innerHTML =
+  "\""+chosen[postChoose]+ "\"" + " -- " + sayer;
+}
+quoteChoose();
+// Make visit counter.
